709484ecb6115371c999feb9d214ab616f09faa4
Harsh Pandhe <harshpandhehome@gmail.com>
2024-11-12 01:43:49 +0530

    Version 01

---
+++ client/src/pages/admin-view/features.jsx
@@ -0,0 +1,5 @@
+function AdminFeatures() {
+  return <div>Admin features</div>;
+}
+
+export default AdminFeatures;
---
+++ client/src/pages/not-found/index.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>Page doesn't exists - 404</div>;
+}
+
+export default NotFound;
